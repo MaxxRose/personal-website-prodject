566ffa58f3bf6c7b48d78d10f97631daee12e625
MaxxRose <90778837+MaxxRose@users.noreply.github.com>
2021-11-01 13:14:46 -0400

    Create yeem.js

---
+++ yeem.js
@@ -0,0 +1,16 @@
+var x = 5;
+var isEven = ((x%2)===0)
+console.log(isEven)
+// Statement 1: The elephant weights less than the mouse
+let eleWeight = 1000;
+let mouseWeight = 2;
+ 
+// Statement 2: The Ostrich is taller than the duck
+let ostrichHeight = 2;
+let duckHeight = 0.3;
+ 
+// Statement 3: The two passwords match
+let pwd1 = 'stromboli';
+let pwd2 = 'stROmBoLi'
+ 
+// Add your code here
