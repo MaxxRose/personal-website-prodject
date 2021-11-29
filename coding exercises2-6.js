5575148aa3a81b333a762e2e2340d5ade430464f
MaxxRose <90778837+MaxxRose@users.noreply.github.com>
2021-11-29 14:03:11 -0500

    nmnmnmnmnmnmnmnmnmn

---
+++ coding exercises2-6.js
@@ -0,0 +1,7 @@
+var myName = "Max";
+if (myName === "Max") {
+    console.log ("Hello Me!");
+}
+else {
+    console.log("Hello Stranger");
+}
---
+++ warmup.js
@@ -0,0 +1,9 @@
+var dogHeight = 17;
+var ratHeight = 24;
+var heightComparison = (dogHeight > ratHeight);
+if (dogHeight > ratHeight) {
+    console.log ("the dog is taller");
+}
+else {
+    console.log("the cat is taller");
+}
